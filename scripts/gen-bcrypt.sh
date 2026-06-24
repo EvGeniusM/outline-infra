aeb30484c126a27a1c12a859d6e2297119adcc4d
EvGeniusM <thezhekychan@gmail.com>
2026-06-24 13:33:06 +0500

    chore: remove comments

--- scripts/gen-bcrypt.sh
+++ scripts/gen-bcrypt.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 PASSWORD="${1:-password}"
-# htpasswd из образа httpd; нормализуем префикс к $2a (его понимает Dex)
 docker run --rm httpd:2 htpasswd -nbBC 10 "" "$PASSWORD" \
   | cut -d: -f2 \
   | sed 's/^\$2y/\$2a/'
--- scripts/gen-secrets.sh
+++ scripts/gen-secrets.sh
@@ -8,11 +8,9 @@ fi
 
 gen() { openssl rand -hex 32; }
 
-# Подставить SECRET_KEY/UTILS_SECRET, если они ещё плейсхолдеры
 for var in SECRET_KEY UTILS_SECRET; do
   if grep -q "^${var}=__GENERATE__" .env; then
     val=$(gen)
-    # sed -i на разных платформах; используем временный файл для переносимости
     sed "s|^${var}=__GENERATE__|${var}=${val}|" .env > .env.tmp && mv .env.tmp .env
     echo "Сгенерирован ${var}"
   fi
